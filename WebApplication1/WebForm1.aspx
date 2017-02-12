9011a69c25b4c742d1cb0a8773c81b74ef41c839
Marat Nabiev <nma2207@gmail.com>
2017-02-12 20:52:52 +0300

    init commit

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Title</title>
+    <link rel="stylesheet" href="StyleSheet.css">
+</head>
+<body>
+    <form id="form1" runat="server">
+    <asp:ScriptManager ID="ScriptManager1" runat="server" /> 
+         <h1><div align="center"> 
+            <asp:UpdatePanel ID="UpdatePanel1" runat="server"> 
+            <ContentTemplate > 
+                <asp:Timer ID="Timer1" runat="server" Interval="599"> 
+                </asp:Timer>
+                <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label> 
+            </ContentTemplate> 
+            </asp:UpdatePanel>
+    </div></h1>
+
+    </form>
+
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        //Timer timer1;
+       static int k = 1;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            Label1.Text = k.ToString();
+            k++;
+            //timer1 = new Timer();
+            //timer1.Interval = 10;
+            //timer1.Enabled = true;
+            //timer1.Tick += timer1_Tick;
+                
+        }
+
+    //    void timer1_Tick(object sender, EventArgs e)
+    //    {
+    //        Label1.Text = DateTime.Now.ToString("hh:mm:ss");
+    //        //throw new NotImplementedException();
+    //    }
+
+    //    protected void Timer1_Tick1(object sender, EventArgs e)
+    //    {
+    //        Label1.Text = DateTime.Now.ToString("hh:mm:ss");
+    //    }
+    }
+}
