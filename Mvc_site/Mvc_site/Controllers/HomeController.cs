47c37f626d91d94900907284daf8060f9ee025e3
Marat Nabiev <nma2207@gmail.com>
2017-02-20 06:21:29 +0300

    Add models, view, controller to my Mvc_site, but it don't work

---
+++ Mvc_site/Mvc_site/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Mvc_site.Models;
+
+namespace Mvc_site.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            IEnumerable<Book> books = db.books;
+            ViewBag.Books = books;
+
+            return View();
+        }
+
+    }
+}
--- Mvc_site/Mvc_site/Models/BookContext.cs
+++ Mvc_site/Mvc_site/Models/BookContext.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
 
 namespace Mvc_site.Models
 {
-    public class BookContext
+    public class BookContext: DbContext
     {
-        
+        public DbSet<Book> books { get; set; }
+        public DbSet<Purchase> purcases { get; set; }
     }
 }
